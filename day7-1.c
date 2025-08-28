0b4a9045891a779b534a6e774a47ba23e1ae7e0c
Abhyudaya Dutt Sharma <dogged.coder@gmail.com>
2025-08-28 20:38:04 +0530

    day 8 added

--- day7-1.c
+++ day7-1.c
@@ -6,8 +6,9 @@ int main()
      int year;
      printf("Enter the year");
      scanf("%d", &year);
-     if (year %4==0)
+     if (year %4==0 || year%400==0)
      printf("Its a leap year");
-     else
-     printf("Its not a leap year")
+     else 
+     printf("Its not a leap year");
+     return 0;
 }
