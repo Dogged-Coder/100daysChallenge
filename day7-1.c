e2cf3da0f26cc41c0d758075d0bddde4e555a27b
Abhyudaya Dutt Sharma <dogged.coder@gmail.com>
2025-08-28 12:20:36 +0530

    hi

---
+++ day7-1.c
@@ -0,0 +1,13 @@
+/*Write a program to input a year and check whether it is a leap year or not using conditional statements.*/
+
+#include<stdio.h>
+int main()
+{
+     int year;
+     printf("Enter the year");
+     scanf("%d", &year);
+     if (year %4==0)
+     printf("Its a leap year");
+     else
+     printf("Its not a leap year")
+}
