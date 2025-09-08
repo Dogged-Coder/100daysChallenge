a763adef52af2a7fbb4b346bf0ffac9be9592ddb
Abhyudaya Dutt Sharma <dogged.coder@gmail.com>
2025-09-08 12:57:13 +0530

    day 16 added

---
+++ day17-1.c
@@ -0,0 +1,12 @@
+/*Write a program to check if a number is an Armstrong number.*/
+#include<stdio.h>
+int main()
+{
+    int num;printf("Rnter a number");
+    scanf("%d",&num);
+    
+}
+
+//lenght
+//each digit
+//power function
