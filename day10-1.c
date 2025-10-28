cb4634e7b5a45ffe2627c21818b977566200ea11
Abhyudaya Dutt Sharma <dogged.coder@gmail.com>
2025-10-28 22:44:29 +0530

    Fix triangle type detection logic

--- day10-1.c
+++ day10-1.c
@@ -10,6 +10,7 @@ int main()
         if(side1==side2==side3)
         printf("This is a equilateral triangle");
         else
+            
         printf("This is a iscosceles triangle");
     }
     else
